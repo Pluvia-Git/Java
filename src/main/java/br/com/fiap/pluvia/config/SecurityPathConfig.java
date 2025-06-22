4a7eb74f8b6976aa2c01f02b9cfb491ab19d123a
joaobroggine <joaovitorbrogginelopes@gmail.com>
2025-06-22 19:24:23 -0300

    Removendo JWT

--- src/main/java/br/com/fiap/pluvia/config/SecurityPathConfig.java
+++ src/main/java/br/com/fiap/pluvia/config/SecurityPathConfig.java
@@ -1,6 +1,5 @@
 package br.com.fiap.pluvia.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,13 +9,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@RequiredArgsConstructor
 public class SecurityPathConfig {
-
-    private final JwtAuthFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -24,16 +19,13 @@ public class SecurityPathConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/usuarios/**").permitAll()
-                        .anyRequest().authenticated()               
+                        .anyRequest().authenticated()
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(httpBasic -> {});
 
-        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-
         return http.build();
     }
-
 
     @Bean
     public UserDetailsService userDetailsService() {
